7b0cb1bf39f9fefd2b91e5d9d37ee43a1ac2e639
khayo254 <khayo1998@gmail.com>
2023-02-16 18:04:50 -0800

    printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stidio.h>
+/**
+ * main - printf function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Writing with proper grammar is like creating a piece of art,\n");
+	return (0);
+}
