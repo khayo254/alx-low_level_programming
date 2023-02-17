4965a10e8232317c3fb078f39855a7ec1c57a09c
khayo254 <khayo1998@gmail.com>
2023-02-16 17:30:22 -0800

    create a c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<sitdio.h>
+
+/**
+* main - Programming is like building a multilingual puzzle
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
