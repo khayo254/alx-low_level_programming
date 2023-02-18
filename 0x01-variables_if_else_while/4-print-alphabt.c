d31f81c02e3822db65353aa92084d350abbbe56d
khayo254 <khayo1998@gmail.com>
2023-02-18 03:44:41 +0300

    all letters except q and e

---
+++ 0x01-variables_if_else_while/4-print-alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (aplhabet = 'a'; alphabet <= 'z', alphabet++)
+	{
+		if (alphabet != 'e' && letter != 'q)
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
