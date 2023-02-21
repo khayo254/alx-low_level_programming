87762be8938eac49e592a1b8bc8a1eeca017a67a
khayo254 <khayo1998@gmail.com>
2023-02-21 20:08:10 +0300

    print lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,12 +12,7 @@ void print_alphabet(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
+
 	}
 	_putchar('\n');
-}
-
-void print_alphabet(void)
-{
-	print_alphabet();
-	return (0);
 }
