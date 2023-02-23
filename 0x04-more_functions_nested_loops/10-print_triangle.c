12a83a7eb369339c325aa566be03418485166833
khayo254 <khayo1998@gmail.com>
2023-02-24 02:06:58 +0300

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,18 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 		int i, j;
 
 		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
