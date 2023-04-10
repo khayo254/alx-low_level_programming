6d0a8b7c34fb0aa24152bcd077c6f8f0ec869ac9
khayo254 <khayo1998@gmail.com>
2023-04-10 14:02:49 -0700

    change binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
- * Return: result - converted number
+ * Return: converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
