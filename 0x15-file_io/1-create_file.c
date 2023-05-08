8b396dff634f204c7091c59b446f9408b59846df
khayo254 <khayo1998@gmail.com>
2023-05-08 08:03:29 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,33 +5,29 @@
  * @filename: the name of the file to create
  * @text_content: the content to write to the file
  *
- * Return: 1 on success,  -1 on failure
+ * Return: 1 on success,  -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int bytes_written;
-	int i;
+	int i = 0;
 
 	if (filename == NULL) /* check for NULL filename */
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '0'; i++)
-			;
-		bytes_written = write(fd, text_content, i);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (i = 0; text_content[i];)
+			i++;
 	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_written = write(fd, text_content, i);
+
+	if (fd == -1 || bytes_written == -1)
+			return (-1);
 
 	close(fd);
+
 	return (1);
 }
