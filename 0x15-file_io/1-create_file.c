bc8e6576b9552120bd54ec77cb6aff3f3fb5d9e1
khayo254 <khayo1998@gmail.com>
2023-05-08 08:11:48 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0')
+		for (i = 0; text_content[i];)
 			i++;
 	}
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes_written = write(fd, text_content, i);
 
