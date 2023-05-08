9c0862d639ed6077dc1e348c0ae279939f4c8ed8
khayo254 <khayo1998@gmail.com>
2023-05-08 07:10:28 -0700

    read and write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,35 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read;
 	ssize_t bytes_written;
 
-	if (filename == NULL) /* check for NULL filename */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	fd = open(filename, O_RDONLY); /* open file for reading */
-	if (fd == -1) /* check for error */
-		return (0);
-
-	buf = malloc(sizeof(char) * letters); /* allocate the buffer */
-	if (buff == NULL) /* check for malloc() error */
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes_read = read(fd, buf, letters); /* read from file */
-	if (bytes_read == -1) /* check for read() error */
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+	buf = malloc(sizeof(char) * letters);
+	bytes_read = read(fd, buf, letters);
 	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	free(buf); /* free buffer */
-	close(fd); /* close file */
-	return (bytes_written); /* return the number of bytes written */
+
+	free(buf);
+	close(fd);
+	return (bytes_written);
 }
